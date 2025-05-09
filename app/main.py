96e1dcac4a9adda78724da69b29f492448cc7df8
qiaoyun680 <549653222@qq.com>
2025-05-09 09:10:08 +0800

    debugger

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import os
 import sys
 import threading
 
-import uvicorn as uvicorn
 from PIL import Image
 from uvicorn import Config
+from uvicorn.server import Server
 
 from app.factory import app
 from app.utils.system import SystemUtils
@@ -19,10 +19,23 @@ from app.core.config import settings
 from app.db.init import init_db, update_db
 
 # uvicorn服务
-Server = uvicorn.Server(Config(app, host=settings.HOST, port=settings.PORT,
-                               reload=settings.DEV, workers=multiprocessing.cpu_count(),
-                               timeout_graceful_shutdown=5))
+# Server = uvicorn.Server(Config(app, host=settings.HOST, port=settings.PORT,
+#                                reload=settings.DEV, workers=multiprocessing.cpu_count(),
+#                                timeout_graceful_shutdown=5))
 
+def run():
+    config = Config(
+        app=app,
+        host=settings.HOST,
+        port=settings.PORT,
+        reload=settings.DEV,
+        workers=multiprocessing.cpu_count(),
+        timeout_graceful_shutdown=5
+    )
+    server = Server(config)
+    # 建议在协程中运行 serve 方法
+    import asyncio
+    asyncio.run(server.serve())
 
 def start_tray():
     """
@@ -78,4 +91,5 @@ if __name__ == '__main__':
     # 更新数据库
     update_db()
     # 启动API服务
-    Server.run()
+    # Server.run()
+    run()
